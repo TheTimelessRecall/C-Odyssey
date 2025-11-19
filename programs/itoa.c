cfad324b85b2511a9b7e5b2f71c76a6a63e27a2a
Anmol Jindal <timelessrecall@gmail.com>
2025-11-19 23:04:44 +0530

    clean up the indentation, added free() to prevent memory leak and match up basic c style

--- programs/itoa.c
+++ programs/itoa.c
@@ -2,64 +2,62 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-void	*memcalloc(size_t nmemb, size_t size)
-{
-	void	*ptr;
-	char	*p;
-	size_t	total;
+void *memcalloc(size_t nmemb, size_t size) {
+    if (size != 0 && nmemb > SIZE_MAX / size)
+        return NULL;
 
-	if (size != 0 && nmemb > SIZE_MAX / size)
-		return (NULL);
-	total = nmemb * size;
-	ptr = malloc(total);
-	if (!ptr)
-		return (NULL);
-	p = (char *)ptr;
-	while (total--)
-		*p++ = 0;
-	return (ptr);
+    size_t total = nmemb * size;
+    void *ptr = malloc(total);
+    if (!ptr)
+        return NULL;
+
+    char *p = ptr;
+    while (total--)
+        *p++ = 0;
+
+    return ptr;
 }
 
-void	*memmcpy(void *dst, const void *src, size_t n)
-{
-	unsigned char			*d;
-	const unsigned char		*s;
+void *memmcpy(void *dst, const void *src, size_t n) {
+    unsigned char *d = dst;
+    const unsigned char *s = src;
 
-	d = (unsigned char *)dst;
-	s = (const unsigned char *)src;
-	while (n--)
-		*d++ = *s++;
-	return (dst);
+    while (n--)
+        *d++ = *s++;
+
+    return dst;
 }
 
-char	*itoa(int n)
-{
-	char	arr[11];
-	char	*out;
-	long	nb;
-	int		i;
+char *itoa(int n) {
+    char arr[11];
+    long nb = n;
+    int i = 10;
 
-	nb = (long)n;
-	i = 10;
-	if (nb < 0)
-		nb *= -1;
-	if (nb == 0)
-		arr[--i] = '0';
-	while (nb)
-	{
-		arr[--i] = (nb % 10) + '0';
-		nb /= 10;
-	}
-	if (n < 0)
-		arr[--i] = '-';
-	out = (char *)memcalloc((10 - i) + 1, sizeof(char));
-	if (!out)
-		return (NULL);
-	memmcpy(out, arr + i, 10 - i);
-	return (out);
+    if (nb < 0)
+        nb = -nb;
+
+    if (nb == 0)
+        arr[--i] = '0';
+
+    while (nb) {
+        arr[--i] = (nb % 10) + '0';
+        nb /= 10;
+    }
+
+    if (n < 0)
+        arr[--i] = '-';
+
+    int len = 10 - i;
+    char *out = memcalloc(len + 1, sizeof(char));
+    if (!out)
+        return NULL;
+
+    memmcpy(out, arr + i, len);
+    return out;
 }
 
-int	main(void)
-{
-	printf("%s\n", itoa(42));
+int main(void) {
+    char *s = itoa(42);
+    printf("%s\n", s);
+    free(s);
 }
